90f95d8e40f7abf6f00b42b99659afcf3a055e77
Steve Magnuson <ag7gn.steve@gmail.com>
2021-12-28 12:20:26 -0800

    Updater after_remove.sh

--- rmsgw/prepost/after_remove.sh
+++ rmsgw/prepost/after_remove.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 # Runs AFTER removing package
+rm -f /usr/share/applications/rmsgw_config_monitor.desktop
+rm -f /usr/bin/rmschanstat.local 
+rm -f /usr/bin/rmsgw-activity.sh 
+rm -f /usr/bin/rmsgw_manager.sh
 exit 0
