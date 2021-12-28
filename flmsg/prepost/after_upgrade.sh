01f9eeea09519512c3f08f6dcdcd18a51aef22a6
Steve Magnuson <ag7gn.steve@gmail.com>
2021-12-28 14:50:11 -0800

    Update flmsg after scripts

--- flmsg/prepost/after_upgrade.sh
+++ flmsg/prepost/after_upgrade.sh
@@ -12,14 +12,14 @@ do
    done
 done
 [ -f /usr/share/applications/flmsg.desktop ] && sudo mv -f /usr/share/applications/flmsg.desktop /usr/share/applications/flmsg.desktop.disabled
-if [ ! -f /usr/share/applications/flmsg-left.desktop ]
+if [ ! -s /usr/share/applications/flmsg-left.desktop ]
 then
    sed -e "s/_LEFT_RADIO_/Left Radio/g" \
-   /usr/share/applications/flmsg-left.template >/usr/share/applications/flmsg-left.desktop 
+   /usr/share/applications/flmsg-left.template >/usr/share/applications/flmsg-left.desktop
 fi
-if [ ! -f /usr/share/applications/flmsg-right.desktop ]
+if [ ! -s /usr/share/applications/flmsg-right.desktop ]
 then
    sed -e "s/_RIGHT_RADIO_/Right Radio/g" \
-   /usr/share/applications/flmsg-right.template >/usr/share/applications/flmsg-right.desktop 
+   /usr/share/applications/flmsg-right.template >/usr/share/applications/flmsg-right.desktop
 fi
 exit 0
