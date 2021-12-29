eea0df9030bb7c3ec59ed187e5d99fa1cd5bf1e4
Steve Magnuson <ag7gn.steve@gmail.com>
2021-12-28 18:28:58 -0800

    Fix hamlib before scripts

--- hamlib/prepost/before_install.sh
+++ hamlib/prepost/before_install.sh
@@ -4,7 +4,7 @@ echo "Remove libhamlib4 libhamlib-utils if installed"
 apt -y remove --purge libhamlib4 libhamlib-utils 2>/dev/null
 echo "Mark libhamlib4 libhamlib-utils as held"
 apt-mark hold libhamlib4 libhamlib-utils
-echo "Remove /usr/lib/hamlib*"
-rm -f /usr/lib/libhamlib*
+#echo "Remove /usr/lib/hamlib*"
+#rm -f /usr/lib/libhamlib*
 echo "Done"
 exit 0
