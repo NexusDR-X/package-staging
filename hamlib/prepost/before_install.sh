8f763a5414e2ef1e56c7067778ff6b8bccbfa54e
Steve Magnuson <ag7gn.steve@gmail.com>
2021-12-29 15:46:18 -0800

    Remove commented out commands

--- hamlib/prepost/before_install.sh
+++ hamlib/prepost/before_install.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env sh
 # Runs BEFORE installing package
-#echo "Remove libhamlib4 libhamlib-utils if installed"
-#dpkg -P libhamlib4 libhamlib-utils 2>/dev/null
-#echo "Mark libhamlib4 libhamlib-utils as held"
-#apt-mark hold libhamlib4 libhamlib-utils
-#echo "Remove /usr/lib/hamlib*"
-#rm -f /usr/lib/libhamlib*
-#echo "Done"
 exit 0
